d9e9afa216501f32b4cd9d447583e048e3317484
akash <mostafamahimakash384@gmail.com>
2021-08-03 23:59:30 +0600

    unit-convert-JS

---
+++ unit-convert.js
@@ -0,0 +1,25 @@
+
+
+InchesToFeet
+function InchesToFeet(inches) {
+    var feet = inches / 12;
+    return feet;
+
+}
+
+var myInches = 144;
+var result = InchesToFeet(myInches);
+console.log("My result is = ", result);
+
+
+var dadiInches = 150;
+var result = InchesToFeet(dadiInches);
+console.log("My Dadir result is = ", result);
+
+
+
+//1. function write
+//2. var declaration
+//3. function call
+//4. return value
+//5.print 
