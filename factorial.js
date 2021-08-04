4f3a89ae929cb413b9076be025f86fa48daad0a1
akash <mostafamahimakash384@gmail.com>
2021-08-05 00:43:21 +0600

    recalculation-Of-Factorial-JS

--- factorial.js
+++ factorial.js
@@ -1,10 +1,24 @@
 
 // 7! calcculation
 
-var factorial = 1;
-for (var i = 1; i <= 3; i++) {
-    console.log(i);
-    factorial = factorial * i;
+// var factorial = 1;
+// for (var i = 1; i <= 3; i++) {
+//     console.log(i);
+//     factorial = factorial * i;
 
+// }
+// console.log("result is: ", factorial);
+
+var factorial = 1;
+function result(number) {
+    for (var i = 1; i <= number; i++) {
+        console.log(i);
+        factorial = factorial * i;
+
+    }
+    return factorial;
 }
-console.log("result is: ", factorial);
+
+var FactirialResult = result(3);
+console.log('Rslt:', FactirialResult);
+
