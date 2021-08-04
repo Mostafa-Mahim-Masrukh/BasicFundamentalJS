cf19212b69d14d72c889db0b0db53f4e5468720f
akash <mostafamahimakash384@gmail.com>
2021-08-05 01:06:00 +0600

    while-loop-factorialJS

---
+++ while-loop-Factorial.js
@@ -0,0 +1,50 @@
+/*
+
+var i = 0;
+while (i < 7) {
+    //statement
+    i++;
+}
+
+*/
+
+
+/*
+
+var factorial = 1;
+function FactorilaCalculation(number) {
+    var i = 1;
+    while (i <= number) {
+        // console.log(i);
+        factorial = factorial * i;
+        i++;
+    }
+    return factorial;
+
+}
+var InputValue = 4;
+var result = FactorilaCalculation(InputValue);
+console.log(result);
+
+*/
+
+
+// 4 * 3 * 2 * 1=24
+
+
+function FactorilaCalculation(number) {
+    var factorial = 1;
+    var i = number;
+    while (i >= 1) {
+        // console.log(i);
+        factorial = factorial * i;
+        i--;
+    }
+    return factorial;
+
+}
+var InputValue = 2;
+var result = FactorilaCalculation(InputValue);
+console.log(result);
+
+
