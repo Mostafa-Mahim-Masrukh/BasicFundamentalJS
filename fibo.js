840813870c200376144f132a6bf8bc1f41b4aa95
akash <mostafamahimakash384@gmail.com>
2021-08-07 11:20:11 +0600

    fibonacci-JS

---
+++ fibo.js
@@ -0,0 +1,16 @@
+function CalculateFibo(numbers) {
+    let result = 0;
+    for (let i = 2; i < 10; i++) {
+        numbers[i] = numbers[i - 1] + numbers[i - 2];
+        // console.log(numbers);
+
+    }
+    return numbers;
+
+
+}
+
+
+var numbers = [1, 0];
+let FinalResult = CalculateFibo(numbers);
+console.log(numbers);
